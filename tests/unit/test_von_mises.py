0cafbaee5bb2893efc1ccdb8619cd2fb2e4dc671
mikewojnowicz <mwojnowi@uci.edu>
2023-05-04 16:21:40 -0600

    add unit test for conversion from angles to points and back

--- tests/unit/test_von_mises.py
+++ tests/unit/test_von_mises.py
@@ -2,7 +2,9 @@ import numpy as np
 
 from dynagroup.von_mises.core import (
     VonMisesModelType,
+    angles_from_points,
     estimate_von_mises_params,
+    points_from_angles,
     sample_from_von_mises_random_walk,
 )
 
@@ -10,8 +12,17 @@ from dynagroup.von_mises.core import (
 def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk():
     T = 1000
     for kappa_true in [1, 10, 100]:
-        print("Now estimating the concentration parameter, kappa, from a von Mises random walk.")
+        print(
+            f"Now estimating the concentration parameter, kappa, from a von Mises random walk where true kappa={kappa_true:.02f}"
+        )
         angles = sample_from_von_mises_random_walk(kappa_true, T, init_angle=0.0)
         params_learned = estimate_von_mises_params(angles, VonMisesModelType.RANDOM_WALK)
         print(f"True kappa: {kappa_true}, Estimated: {params_learned.kappa:.02f}")
         assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
+
+
+def test__points_from_angles__then__angles_from_points():
+    angles = np.array([-1, -0.5, 0, 0.5, 1]) * np.pi
+    points = points_from_angles(angles)
+    angles_reconstructed = angles_from_points(points)
+    assert np.allclose(angles, angles_reconstructed)

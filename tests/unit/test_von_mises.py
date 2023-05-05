dd898ee342bae48a3935b51730d07749c0216f8c
mikewojnowicz <mwojnowi@uci.edu>
2023-05-05 08:20:38 -0600

    added unit test for estimating params of von mises random walk with drift.

--- tests/unit/test_von_mises.py
+++ tests/unit/test_von_mises.py
@@ -6,19 +6,8 @@ from dynagroup.von_mises.core import (
     estimate_von_mises_params,
     points_from_angles,
     sample_from_von_mises_random_walk,
+    sample_from_von_mises_random_walk_with_drift,
 )
-
-
-def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk():
-    T = 1000
-    for kappa_true in [1, 10, 100]:
-        print(
-            f"Now estimating the concentration parameter, kappa, from a von Mises random walk where true kappa={kappa_true:.02f}"
-        )
-        angles = sample_from_von_mises_random_walk(kappa_true, T, init_angle=0.0)
-        params_learned = estimate_von_mises_params(angles, VonMisesModelType.RANDOM_WALK)
-        print(f"True kappa: {kappa_true}, Estimated: {params_learned.kappa:.02f}")
-        assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
 
 
 def test__points_from_angles__then__angles_from_points():
@@ -26,3 +15,29 @@ def test__points_from_angles__then__angles_from_points():
     points = points_from_angles(angles)
     angles_reconstructed = angles_from_points(points)
     assert np.allclose(angles, angles_reconstructed)
+
+
+def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk():
+    T = 1000
+    print("Testing inference for von mises random walk WITHOUT drift.")
+    for kappa_true in [1, 10, 100]:
+        angles = sample_from_von_mises_random_walk(kappa_true, T, init_angle=0.0)
+        params_learned = estimate_von_mises_params(angles, VonMisesModelType.RANDOM_WALK)
+        print(f"True kappa: {kappa_true:.02f}, Estimated: {params_learned.kappa:.02f}")
+        assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
+
+
+def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk_with_drift():
+    T = 1000
+    true_drift_angle = np.pi / 4
+    init_angle = 0.0
+    print("Testing inference for von mises random walk WITH drift.")
+    for kappa_true in [1, 10, 100]:
+        angles = sample_from_von_mises_random_walk_with_drift(
+            kappa_true, T, init_angle, true_drift_angle
+        )
+        params_learned = estimate_von_mises_params(angles, VonMisesModelType.RANDOM_WALK_WITH_DRIFT)
+        print(f"True kappa: {kappa_true:.02f}, Estimated: {params_learned.kappa:.02f}")
+        print(f"True drift: {true_drift_angle:.02f}, Estimated: {params_learned.drift:.02f}")
+        assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
+        assert np.isclose(params_learned.drift, true_drift_angle, rtol=0.20)

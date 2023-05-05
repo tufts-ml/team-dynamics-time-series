b2f0c137a2bb110983647a6e3c479e258857d741
mikewojnowicz <mwojnowi@uci.edu>
2023-05-05 10:00:57 -0600

    * Unified attributes of VonMisesParams -> don't need separate "drift" and "loc" parameters. It's the same equation, but with different interps in different contexts. * Converted VonMisesParams to a jax dataclass. * Added unit test for the IID model.

--- tests/unit/test_von_mises.py
+++ tests/unit/test_von_mises.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import vonmises
 
 from dynagroup.von_mises.core import (
     VonMisesModelType,
@@ -17,6 +18,19 @@ def test__points_from_angles__then__angles_from_points():
     assert np.allclose(angles, angles_reconstructed)
 
 
+def test_that__estimate_von_mises_params__gives_approximately_correct_parameters_for_iid_von_mises_model():
+    T = 1000
+    print("Testing inference for iid von mises model.")
+    for kappa_true in [1, 10, 100]:
+        for loc_true in np.array([0, -0.5, 0.5]) * np.pi:
+            angles = vonmises.rvs(kappa_true, loc=loc_true, size=T)
+            params_learned = estimate_von_mises_params(angles, VonMisesModelType.IID)
+            print(f"True loc: {loc_true:.02f}, Estimated: {params_learned.loc:.02f}")
+            print(f"True kappa: {kappa_true:.02f}, Estimated: {params_learned.kappa:.02f}")
+            assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
+            assert np.isclose(params_learned.loc, loc_true, atol=np.pi / 16)
+
+
 def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk():
     T = 1000
     print("Testing inference for von mises random walk WITHOUT drift.")
@@ -27,7 +41,7 @@ def test_that__estimate_von_mises_params__gives_approximately_correct_concentrat
         assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
 
 
-def test_that__estimate_von_mises_params__gives_approximately_correct_concentration_parameter_for_a_von_mises_random_walk_with_drift():
+def test_that__estimate_von_mises_params__gives_approximately_correct_parameters_for_a_von_mises_random_walk_with_drift():
     T = 1000
     true_drift_angle = np.pi / 4
     init_angle = 0.0
@@ -38,6 +52,6 @@ def test_that__estimate_von_mises_params__gives_approximately_correct_concentrat
         )
         params_learned = estimate_von_mises_params(angles, VonMisesModelType.RANDOM_WALK_WITH_DRIFT)
         print(f"True kappa: {kappa_true:.02f}, Estimated: {params_learned.kappa:.02f}")
-        print(f"True drift: {true_drift_angle:.02f}, Estimated: {params_learned.drift:.02f}")
+        print(f"True drift: {true_drift_angle:.02f}, Estimated: {params_learned.loc:.02f}")
         assert np.isclose(params_learned.kappa, kappa_true, rtol=0.20)
-        assert np.isclose(params_learned.drift, true_drift_angle, rtol=0.20)
+        assert np.isclose(params_learned.loc, true_drift_angle, rtol=0.20)

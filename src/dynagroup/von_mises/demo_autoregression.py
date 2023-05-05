43c43a0a69c6bbb304f48c0ad13769f83dc5415e
mikewojnowicz <mwojnowi@uci.edu>
2023-05-05 14:36:26 -0600

    * Added demo for sampling + inference for von mises autoregression.  Note that for some combinations of parameters, inference is bad. I think I still need to work on initialization. * Added general parameter inference for von mises autoregression. * (maint) Made kappa estimation for other methods -- IID and random walk (both with and without drift) -- depend on this more general form.  The more general form is more illuminating, since it depends on the mean. Unit test still passes.

---
+++ src/dynagroup/von_mises/demo_autoregression.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from dynagroup.von_mises.generate import sample_from_von_mises_AR_with_drift
+from dynagroup.von_mises.inference import VonMisesModelType, estimate_von_mises_params
+from dynagroup.von_mises.util import points_from_angles
+
+
+###
+# Configs
+###
+
+T = 1000
+kappa_true = 100
+plot_points_on_circle = True
+plot_angle_time_series = True
+
+###
+# Inference
+###
+
+
+### Suspected Limitations:
+# Can't reliably do inference if ...
+# 1) true ar coef is exactly 1 or -1. This is possibly
+# due to the hyperbolic tangent tranformation.  If we want to have a random walk or random
+# walk with drift for sure, maybe use dedicated inference for those models.
+# 2) drift angle is too big (like over -.5)
+
+for drift_true in np.array([-0.5, -0.25, 0.0, 0.25, 0.5]) * np.pi:
+    for ar_coef_true in [-0.9, -0.5, 0.0, 0.5, 0.9]:
+        print("\n --- New test ---")
+        print(f"ar coef true:{ar_coef_true:.02f}, drift true:{drift_true:.02f}")
+
+        init_angle = drift_true
+        angles = sample_from_von_mises_AR_with_drift(
+            kappa_true, T, ar_coef_true, init_angle, drift_true
+        )
+        points = points_from_angles(angles)
+
+        if plot_angle_time_series:
+            T_to_plot = 100
+            plt.scatter(
+                np.arange(T_to_plot), angles[-T_to_plot:], c=np.arange(T_to_plot), cmap="cool"
+            )
+            plt.xlabel("time")
+            plt.ylabel(r"angle $\in [-\pi, \pi]$")
+            plt.tight_layout()
+            plt.show()
+
+        if plot_points_on_circle:
+            T_to_plot = 25
+            plt.scatter(
+                points[-T_to_plot:, 0], points[-T_to_plot:, 1], c=np.arange(T_to_plot), cmap="cool"
+            )
+            plt.title(
+                f"Last 25 samples of a von Mises AR with drift \n (drift angle={drift_true:.02f}, ar_coef = {ar_coef_true:02f}, kappa={kappa_true:.02f})"
+            )
+            plt.xlim(-1.1, 1.1)
+            plt.ylim(-1.1, 1.1)
+            plt.show()
+
+        params = estimate_von_mises_params(angles, VonMisesModelType.AUTOREGRESSION)
+
+        print(f"ar coef true:{ar_coef_true:.02f}, learned:{params.ar_coef:.02f}")
+        print(f"drift true:{drift_true:.02f}, learned:{params.drift:.02f}")
+        print(f"kappa true:{kappa_true:.02f}, learned:{params.kappa:.02f}")
+        assert np.isclose(params.ar_coef, ar_coef_true, atol=0.20)
+        assert np.isclose(params.kappa, kappa_true, rtol=0.20)
+        assert np.isclose(params.drift, drift_true, atol=np.pi / 16)
--- src/dynagroup/von_mises/inference.py
+++ src/dynagroup/von_mises/inference.py
@@ -1,16 +1,19 @@
 import functools
 import math
-from typing import Optional
+import warnings
+from typing import Optional, Tuple
 
 import jax.numpy as jnp
 import jax_dataclasses as jdc
 import numpy as np
 import scipy
 from dynamax.utils.optimize import run_gradient_descent
+from numpy import arctanh, tanh
 from scipy import stats
 from scipy.special import (  # modified bessel of first kind
     iv as modified_bessel_first_kind,
 )
+from sklearn.linear_model import LinearRegression
 
 from dynagroup.types import NumpyArray1D
 from dynagroup.util import make_2d_rotation_JAX
@@ -32,14 +35,33 @@ class VonMisesModelType:
     """
     Values:
         IID
-            x_t ~ VonMises(loc, kappa)
+            theta_t ~ VonMises(loc, kappa)
+                for kappa >=0, loc in [-pi, pi]
         Random Walk
-            x_t ~ VonMises(x_{t-1}, kappa)
+            theta_t ~ VonMises(theta_{t-1}, kappa)
+                for kappa >=0
+        Random Walk with Drift
+            theta_t ~ VonMises(theta_{t-1} + + angular_drift, kappa)
+                for angular_drift in [-pi, pi], kappa >=0,
+        AUTOREGRESSION
+            theta_t ~ VonMises( alpha *theta_{t-1} + angular_drift, kappa)
+                for alpha in [-1,1], angular_drift in [-pi, pi], kappa >=0
+
+    Note that the AUTOREGRESSION model generalizes all of the model types
+        - If ar_coef==1 and drift!=0, then this is a random walk with drift.
+        - If ar_coef==1 and drift_angle==0, then this is a random walk without drift.
+        - If ar_coef==0, then this gives IID samples with location=drift.
+
+    However we allow specification of special cases to aid in inference.
+    For example, parameter inference on the full AR model, at least in the implementation as of 5/5/23, can become
+    dicey when the angular_drift is large (greater than |pi/2| ) or when the AUTOREGRESSION coefficient
+    is exactly 1.0 or -1.0
     """
 
     IID = 1
     RANDOM_WALK = 2
     RANDOM_WALK_WITH_DRIFT = 3
+    AUTOREGRESSION = 4
 
 
 @jdc.pytree_dataclass
@@ -135,41 +157,67 @@ def _estimate_kappa_for_general_von_mises_model(RHS: float) -> float:
     return scipy.optimize.fsolve(this_equation, kappa_init)[0]
 
 
-def estimate_kappa_for_iid_samples(angles: np.array):
+def estimate_kappa_for_iid_samples_USING_THE_MINOR_BANERJEE_SIMPLIFICATION(
+    angles: np.array,
+) -> float:
     """
     We find the root of an equation whose root gives the MLE for the concentration parameter, kappa,
     given iid samples from a Von Mises distribution (on the circle).  The computation of this equation can be obtained
     by the argument in Appendix A.1, Banerjee et al 2005 JMLR,  Clustering on the Unit Hypersphere using von Mises-Fisher Distributions.
+
+    Remarks:
+        The minor banerjee simplification just involves a cancelation between the numerator and the denominator.
+        We leave `estimate_kappa_for_iid_samples` as the standard (unmarked) function because it utilizes a more
+        obviously generalizable presentation of the logic in the function body
     """
     norm_of_cartesian_mean = _compute_norm_of_mean_Cartesian_coordinate(angles)
     return _estimate_kappa_for_general_von_mises_model(norm_of_cartesian_mean)
 
 
-def estimate_kappa_for_random_walk(angles: np.array):
+def estimate_kappa_for_iid_samples(angles: np.array, loc_estimate: float) -> float:
+    """
+    We find the root of an equation whose root gives the MLE for the concentration parameter, kappa,
+    given iid samples from a Von Mises distribution (on the circle).  The computation of this equation can be obtained
+    by the argument in Appendix A.1, Banerjee et al 2005 JMLR,  Clustering on the Unit Hypersphere using von Mises-Fisher Distributions.
+    """
+    return estimate_kappa_for_autoregression(angles, drift_angle=loc_estimate, ar_coef=0.0)
+
+
+def estimate_kappa_for_random_walk(angles: np.array) -> float:
     """
     We find the root of an equation whose root gives the MLE for the concentration parameter, kappa,
     of a Von Mises random walk.  The computation of this equation can be obtained from MTW's notes,
     and/or by a slight tweak to the argument in Appendix A.1, Banerjee et al 2005 JMLR,
     Clustering on the Unit Hypersphere using von Mises-Fisher Distributions.
     """
-    points = points_from_angles(angles)
-
-    # TODO: We are ignoring the first measurement and estimating this from T-1 measurements.
-    # We could also posit an initial measurement x_0 and then estimate this from T measurements.
-
-    RHS = np.mean(np.einsum("td,td->t", points[1:], points[:-1]))
-    return _estimate_kappa_for_general_von_mises_model(RHS)
+    return estimate_kappa_for_autoregression(angles, drift_angle=0.0, ar_coef=1.0)
 
 
-def estimate_kappa_for_random_walk_with_drift(angles: np.array, drift_angle: float):
+def estimate_kappa_for_random_walk_with_drift(angles: np.array, drift_angle: float) -> float:
     """
     We find the root of an equation whose root gives the MLE for the concentration parameter, kappa,
     of a Von Mises random walk with drift.  The computation of this equation can be obtained from MTW's notes,
     and/or by a slight tweak to the argument in Appendix A.1, Banerjee et al 2005 JMLR,
     Clustering on the Unit Hypersphere using von Mises-Fisher Distributions.
     """
+    return estimate_kappa_for_autoregression(angles, drift_angle, ar_coef=1.0)
+
+
+def estimate_kappa_for_autoregression(
+    angles: np.array, drift_angle: float, ar_coef: float
+) -> float:
+    """
+    We find the root of an equation whose root gives the MLE for the concentration parameter, kappa,
+    of a Von Mises autoregression.  The computation of this equation can be obtained from MTW's notes,
+    and/or by a slight tweak to the argument in Appendix A.1, Banerjee et al 2005 JMLR,
+    Clustering on the Unit Hypersphere using von Mises-Fisher Distributions.
+
+    Arguments:
+        drift_angle: drift in terms of an angle in [-pi, pi]
+        ar_coef: autoregressive coefficient on the previous angle, in [-1,1]
+    """
     observed_points = points_from_angles(angles)
-    mean_angles = angles[:-1] + drift_angle
+    mean_angles = ar_coef * angles[:-1] + drift_angle
     mean_points = points_from_angles(mean_angles)
 
     # TODO: We are ignoring the first measurement and estimating this from T-1 measurements.
@@ -177,6 +225,11 @@ def estimate_kappa_for_random_walk_with_drift(angles: np.array, drift_angle: flo
 
     RHS = np.mean(np.einsum("td,td->t", observed_points[1:], mean_points))
     return _estimate_kappa_for_general_von_mises_model(RHS)
+
+
+###
+# General function!
+###
 
 
 def estimate_von_mises_params(
@@ -192,9 +245,9 @@ def estimate_von_mises_params(
     if model_type == VonMisesModelType.IID:
         # Compute the MoM estimate for the mean direction
         loc = stats.circmean(angles, high=np.pi, low=-np.pi)
-        kappa = estimate_kappa_for_iid_samples(angles)
+        kappa = estimate_kappa_for_iid_samples(angles, loc)
         # TODO: Is it weird to use MOM for one estimate and ML for another?
-        return VonMisesParams(loc, kappa)
+        return VonMisesParams(loc, kappa=kappa)
     elif model_type == VonMisesModelType.RANDOM_WALK:
         drift = 0.0
         kappa = estimate_kappa_for_random_walk(angles)
@@ -203,12 +256,18 @@ def estimate_von_mises_params(
         drift = estimate_drift_angle_for_von_mises_random_walk_with_drift(angles)
         kappa = estimate_kappa_for_random_walk_with_drift(angles, drift)
         return VonMisesParams(drift, kappa=kappa)
+    elif model_type == VonMisesModelType.AUTOREGRESSION:
+        drift, ar_coef = estimate_drift_angle_and_ar_coef_for_von_mises_ar_with_drift(
+            angles, num_coord_ascent_iterations=10
+        )
+        kappa = estimate_kappa_for_autoregression(angles, drift, ar_coef)
+        return VonMisesParams(drift, kappa, ar_coef)
     else:
         raise ValueError("What model type do you want?")
 
 
 ###
-# Inference for Von Mises random walk WITH drift
+# Inference for drift angle for Von Mises random walk WITH drift
 ###
 
 
@@ -308,3 +367,89 @@ def compute_mean_angle_between_neighbors(angles):
     points = points_from_angles(angles)
     neighbor_angles = [_angle_between_zero_and_2pi(x, y) for (x, y) in zip(points[1:], points[:-1])]
     return np.mean(neighbor_angles)
+
+
+###
+# Inference for drift angle and AUTOREGRESSION coefficient for Von Mises autoregression
+###
+
+
+def equation_whose_root_is_the_drift_MLE_given_ar_coef(drift, ar_coef, angles):
+    points = points_from_angles(angles)
+    result = 0.0
+    T = len(points)
+    for t in range(T):
+        result += -points[t, 0] * np.sin(drift + ar_coef * angles[t - 1]) + points[t, 1] * np.cos(
+            drift + ar_coef * angles[t - 1]
+        )
+    return result
+
+
+def equation_whose_root_is_the_arctanh_ar_coef_MLE_given_drift(arctanh_ar_coef, drift, angles):
+    ar_coef = tanh(arctanh_ar_coef)
+    points = points_from_angles(angles)
+    result = 0.0
+    T = len(points)
+    for t in range(T):
+        result += -points[t, 0] * angles[t - 1] * np.sin(drift + ar_coef * angles[t - 1]) + points[
+            t, 1
+        ] * angles[t - 1] * np.cos(drift + ar_coef * angles[t - 1])
+    return result
+
+
+def estimate_drift_angle_and_ar_coef_for_von_mises_ar_with_drift(
+    angles: NumpyArray1D,
+    num_coord_ascent_iterations: int,
+    verbose=False,
+) -> Tuple[float, float]:
+    """
+    Do inference on drift (rotation angle) for Von Mises Random Walk with Drift.
+    Uses gradient descent
+
+    Arguments:
+        optimizer_init_strategy: str, in ["smart", "zero"].
+            "zero" does not work well when the true angle is large; it's just kept here for the purposes of demonstrating
+            that gradient descent is sensitive to inits (or possibly just need to run it longer.)
+    """
+    warnings.warn(
+        f" The strategy used here is numerical optimization.  It appears to struggle when the drift angle is too "
+        f"large (like >|pi/2|) and/or when the ar coefficient is EXACTLY 1.0 or -1.0.  The later problem is likely "
+        f"caused by the fact that we use the hyperbolic tangent to map from [-1,1] to unconstrained space. "
+        f"If the AR coefficient = 1.0 exactly, we have a random walk (with or without drift); there are separate "
+        f"inference functions for those models that are more reliable.  Just specify the appropriate Model in the "
+        f"model enum. Note that in these cases the initialization can be WAY off.  Perhaps the problem is with angle wrapping?"
+    )
+
+    ###
+    # Smart Init via Linear Regression
+    ###
+
+    reg = LinearRegression().fit(angles[:-1][:, None], angles[1:])
+    ar_coef = reg.coef_[0]
+    drift = reg.intercept_
+
+    print(f"After initialization: ar_coef:{ar_coef:.02f}, drift:{drift:.02f}")
+
+    ###
+    # Do inference on drift (rotation angle) for Von Mises Random Walk with Drift
+    ###
+    for it in range(num_coord_ascent_iterations):
+        this_drift_equation = functools.partial(
+            equation_whose_root_is_the_drift_MLE_given_ar_coef,
+            ar_coef=ar_coef,
+            angles=angles,
+        )
+        drift = scipy.optimize.fsolve(this_drift_equation, drift)[0]
+        this_ar_coef_equation = functools.partial(
+            equation_whose_root_is_the_arctanh_ar_coef_MLE_given_drift,
+            drift=drift,
+            angles=angles,
+        )
+        arctanh_ar_coef = scipy.optimize.fsolve(this_ar_coef_equation, arctanh(ar_coef))[0]
+        ar_coef = tanh(arctanh_ar_coef)
+        if verbose:
+            print(
+                f"it: {it+1}/{num_coord_ascent_iterations}, drift:{drift:.02f}, ar_coef:{ar_coef:.02f}"
+            )
+
+    return drift, ar_coef

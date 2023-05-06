a9c576bedafc864a4c7b58e065f7d82d6067fd9e
mikewojnowicz <mwojnowi@uci.edu>
2023-05-06 11:07:42 -0600

    * Added sample weights to inference of ar_coef and drift for von mises autoregression. * (bugfix) inference for ar coef and drift had bug, it was using the LAST angle to determine the mean for the ZEROTH angle!   Instead, the zeroth angle should have just been skipped in this inference. * (bugfix) unit test had bug, was testing kappa twice instead of kappa and ar coef

--- tests/unit/test_von_mises.py
+++ tests/unit/test_von_mises.py
@@ -62,7 +62,6 @@ def test_that__estimate_von_mises_params__gives_approximately_correct_parameters
 
 def test_that__estimate_von_mises_params__gives_approximately_correct_parameters_for_a_von_mises_autoregression():
     T = 500
-
     drift_angles_true = np.array([-0.25, 0.0, 0.25]) * np.pi
 
     kappa_true = 100.0
@@ -83,6 +82,50 @@ def test_that__estimate_von_mises_params__gives_approximately_correct_parameters
             print(f"ar coef true:{ar_coef_true:.02f}, learned:{params_learned.ar_coef:.02f}")
             print(f"True kappa: {kappa_true:.02f}, Estimated: {params_learned.kappa:.02f}")
             print(f"True drift: {drift_angle_true:.02f}, Estimated: {params_learned.drift:.02f}")
-            assert np.isclose(params_learned.kappa, kappa_true, rtol=0.30)
+            assert np.isclose(params_learned.ar_coef, ar_coef_true, atol=0.30)
             assert two_angles_are_close(params_learned.drift, drift_angle_true, atol=np.pi / 8)
             assert np.isclose(params_learned.kappa, kappa_true, rtol=0.30)
+
+
+def test_that__estimate_von_mises_params__handles_sample_weights__for_von_mises_autoregression():
+    """
+    We create a dataset whose first half is very different from the second half.
+    We then see if we get very similar results when:
+        - we omit the second half
+        - we analyze the whole dataset, but put all the sample weight on the first half.
+    """
+    T_half = 100
+
+    first_angles = sample_from_von_mises_AR_with_drift(
+        kappa=10, T=T_half, ar_coef=0.2, init_angle=0.0, drift_angle=np.pi / 8
+    )
+    second_angles = sample_from_von_mises_AR_with_drift(
+        kappa=100, T=T_half, ar_coef=-0.5, init_angle=np.pi, drift_angle=-np.pi / 8
+    )
+    angles = np.hstack((first_angles, second_angles))
+
+    print("Testing use of sample weights when doing inference with von mises autoregression.")
+
+    params_learned_first_half_via_sample_weights = estimate_von_mises_params(
+        angles,
+        VonMisesModelType.AUTOREGRESSION,
+        sample_weights=np.hstack((np.ones(T_half), np.zeros(T_half))),
+    )
+    params_learned_first_half_via_omission = estimate_von_mises_params(
+        angles[:T_half], VonMisesModelType.AUTOREGRESSION
+    )
+    assert two_angles_are_close(
+        params_learned_first_half_via_omission.drift,
+        params_learned_first_half_via_sample_weights.drift,
+        atol=np.pi / 32,
+    )
+    assert np.isclose(
+        params_learned_first_half_via_omission.ar_coef,
+        params_learned_first_half_via_sample_weights.ar_coef,
+        rtol=0.05,
+    )
+    assert np.isclose(
+        params_learned_first_half_via_omission.kappa,
+        params_learned_first_half_via_sample_weights.kappa,
+        rtol=0.05,
+    )

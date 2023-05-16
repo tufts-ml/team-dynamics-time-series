(bugfix) my code for normalizing the bb data was WRONG! it was doubling both x's and y's by 2.  and my recent attempt to flip the x's at halftime wasn't doing it right (so i was getting negative numbrs, instead of numbers in [0,2]).

src/dynagroup/model2a/basketball/get_data.py
@@ -11,7 +11,7 @@ from dynagroup.types import NumpyArray1D, NumpyArray3D
 ###
 @dataclass
 class Data:
-    xs: NumpyArray3D  # (T_grand,J,D)
+    positions: NumpyArray3D  # (T_grand,J,D)
     event_end_times: NumpyArray1D  # (E,)
     has_ball_team: NumpyArray1D  # (T_grand, )
 
@@ -21,14 +21,14 @@ def get_data() -> Data:
     # Load Raw Data
     ###
 
-    # xs_raw are (T_grand,J,D), and involve vertically stacking disjoint basketball plays for the
+    # positions_raw are (T_grand,J,D), and involve vertically stacking disjoint basketball plays for the
     # J=5 basketball players on TOR.  D=2 gives the (x,y) coordinates on the court
 
-    # covariates are (T_grand,M). The first two columns give the (x,y) coords of the ball. The third column
+    # covariates are (T_grand,M). The first two columns give the (x,y) positions of the ball. The third column
     # gives its radius (when in the air, the radius gets larger.)
     DATA_LOAD_DIR = "/Users/mwojno01/Repos/dynagroup/data/basketball/"
     POST_HALFTIME_TIMESTEP = 13841  # Visually extracted from filenames, below.
-    xs_raw = np.load(
+    positions_raw = np.load(
         DATA_LOAD_DIR
         + "basketball_game_TOR_vs_CHA_post_halftime_timestep_13841_stacked_starter_plays.npy"
     )
@@ -45,11 +45,11 @@ def get_data() -> Data:
 
     ### Handle NANs
     # Patch up the few nans  (41/21467) that for some reason only exist in player 0's data
-    T, J, D = np.shape(xs_raw)
+    T, J, D = np.shape(positions_raw)
     j = 0
     for t in range(T):
-        if np.isnan(xs_raw[t, j, 0]):
-            xs_raw[t, j, :] = xs_raw[t - 1, j, :]
+        if np.isnan(positions_raw[t, j, 0]):
+            positions_raw[t, j, :] = positions_raw[t - 1, j, :]
 
     ### Normalize Basketball coordinates
     # We want x to be in [0,2] and y to be in [0,1]
@@ -58,35 +58,55 @@ def get_data() -> Data:
     X_MAX = 100
     Y_MAX = 50
 
-    xs_normalized_but_not_aligned_for_halftime = copy.copy(xs_raw)
-    xs_normalized_but_not_aligned_for_halftime[:, :, 0] /= X_MAX
-    xs_normalized_but_not_aligned_for_halftime[:, :, 1] /= Y_MAX
+    APPROX_X_MIN_AFTER_NORMALIZATION = 0
+    APPROX_X_MAX_AFTER_NORMALIZATION = 2
+    APPROX_Y_MIN_AFTER_NORMALIZATION = 0
+    APPROX_Y_MAX_AFTER_NORMALIZATION = 1
 
-    xs_normalized_but_not_aligned_for_halftime[:, :, 0] *= 2  # let first dim be twice as big..
+    positions_normalized_but_not_aligned_for_halftime = copy.copy(positions_raw)
+    positions_normalized_but_not_aligned_for_halftime[:, :, 0] /= X_MAX
+    positions_normalized_but_not_aligned_for_halftime[:, :, 1] /= Y_MAX
+
+    positions_normalized_but_not_aligned_for_halftime[
+        :, :, 0
+    ] *= 2  # let first dim be twice as big..
 
     ### "Flip" the x coordinates (length of court) at half time,
     # since the team is traveling in the opposite direction as before when they are on offense
-    xs = xs_normalized_but_not_aligned_for_halftime
-    xs[POST_HALFTIME_TIMESTEP:] = (
-        1.0 - xs_normalized_but_not_aligned_for_halftime[POST_HALFTIME_TIMESTEP]
+
+    positions = positions_normalized_but_not_aligned_for_halftime
+    positions[POST_HALFTIME_TIMESTEP:, :, 0] = (
+        APPROX_X_MAX_AFTER_NORMALIZATION
+        - positions_normalized_but_not_aligned_for_halftime[POST_HALFTIME_TIMESTEP:, :, 0]
     )
+
+    # Rk: Tolerances are loose because (a) raw data has a xval somewhere of -2 (bug? out of bounds?)
+    # and (b) other that that, the maxima and minima need not be actually attained.
+    if not np.isclose(np.min(positions[:, :, 0]), APPROX_X_MIN_AFTER_NORMALIZATION, atol=0.05):
+        raise RuntimeError
+    if not np.isclose(np.max(positions[:, :, 0]), APPROX_X_MAX_AFTER_NORMALIZATION, atol=0.05):
+        raise RuntimeError
+    if not np.isclose(np.min(positions[:, :, 1]), APPROX_Y_MIN_AFTER_NORMALIZATION, atol=0.05):
+        raise RuntimeError
+    if not np.isclose(np.max(positions[:, :, 1]), APPROX_Y_MAX_AFTER_NORMALIZATION, atol=0.05):
+        raise RuntimeError
 
     ###
     # Process the covariates
     ###
 
-    T_grand, J = np.shape(xs)[:2]
+    T_grand, J = np.shape(positions)[:2]
 
     # if distances<1 and radius <6, player has ball
     has_ball_players = np.zeros((T_grand, J), dtype=int)
     for t in range(T_grand):
         DISTANCE_CUTOFF_FOR_HAVING_BALL = (
-            3.0  # Determined by EDA.  Works for xs_raw, not normalized xs!!!
+            3.0  # Determined by EDA.  Works for positions_raw, not normalized positions!!!
         )
         RADIUS_CUTOFF_FOR_HAVING_BALL = 8.0  # Determined by EDA
         ball_location = covariates_raw[t, :2]
         ball_radius = covariates_raw[t, 2]
-        player_locations = xs_raw[t]
+        player_locations = positions_raw[t]
         distances = np.linalg.norm(player_locations - ball_location, axis=1)
         has_ball_players_bool = (distances < DISTANCE_CUTOFF_FOR_HAVING_BALL) * (
             ball_radius < RADIUS_CUTOFF_FOR_HAVING_BALL
@@ -113,4 +133,4 @@ def get_data() -> Data:
     System level covariates
         * team has ball
     """
-    return Data(xs, event_end_times, has_ball_team)
+    return Data(positions, event_end_times, has_ball_team)
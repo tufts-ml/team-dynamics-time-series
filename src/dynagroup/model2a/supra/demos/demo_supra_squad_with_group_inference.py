4b56c10efc4409022c83f3de2a393d251a956880
mikewojnowicz <mwojnowi@uci.edu>
2023-05-15 22:10:41 -0600

    various changes during exploration

--- src/dynagroup/model2a/supra/demos/demo_supra_squad_with_group_inference.py
+++ src/dynagroup/model2a/supra/demos/demo_supra_squad_with_group_inference.py
@@ -44,14 +44,19 @@ from dynagroup.vi.core import SystemTransitionPrior_JAX, run_CAVI_with_JAX
 ### Events
 event_end_times = None
 
+### Data Snippet
+show_plots_of_data = False
+starting_minute = 0
+ending_minute = 1
+
 ### Model specification
 num_entity_regimes = 4
 num_system_regimes = 3
-alpha_system_prior, kappa_system_prior = 1.0, 10.0
+alpha_system_prior, kappa_system_prior = 1.0, 50.0
 system_covariate_type = "running_vulnerability_to_north"  # ["four_security", "north_security", "running_vulnerablity_to_north"]
 
 ### Initialization
-show_plots_after_init = True
+show_plots_after_init = False
 bottom_half_self_transition_prob = 0.995
 bottom_half_changepoint_penalty = 10.0
 bottom_half_min_segment_size = 10
@@ -60,10 +65,11 @@ top_half_num_EM_iterations = 20
 initialization_seed = 0
 
 ### Diagnostics
-save_dir = "/Users/mwojno01/Desktop/TRY_running_vulnerability_to_north_as_covariate_L=3_and_kappa=10_one_CAVI_it/"
+save_dir = "/Users/mwojno01/Desktop/TRY_small_dataset_with_running_vulnerablity_and_dest_bias_via_recurrence/"
 
 ### Inference
-n_cavi_iterations = 1
+show_plots_after_learning = False
+n_cavi_iterations = 10
 M_step_toggle_for_STP = "gradient_descent"
 # can i do M_step_toggle_for_ETP in closed form, even though there are L of them?
 M_step_toggle_for_ETP = "gradient_descent"
@@ -87,24 +93,26 @@ if not "df" in globals():
 
 time_snippet = make_time_snippet_based_on_desired_elapsed_secs(
     df,
-    elapsed_secs_after_contact_start_for_starting=(0 * 60.0),
-    elapsed_secs_after_start_for_snipping=(1 * 60.0),
+    elapsed_secs_after_contact_start_for_starting=(starting_minute * 60.0),
+    elapsed_secs_after_start_for_snipping=(ending_minute * 60.0),
     timestep_every=20,
 )
 
 snip = make_data_snippet(df, time_snippet)
 
-polar_plot_the_squad_headings(snip.squad_angles, snip.clock_times, save_dir, show_plot=True)
+polar_plot_the_squad_headings(
+    snip.squad_angles, snip.clock_times, save_dir, show_plot=show_plots_of_data
+)
 
 
-if system_covariate_type == "four_security":
+if system_covariate_type == "north_security":
     INDEX_OF_NORTH_DIRECTION = 0
     system_covariates_to_plot = snip.system_covariates_zero_to_hundred[:, INDEX_OF_NORTH_DIRECTION][
         :, None
     ]
     system_covariates = normalize_matrix_by_mean_and_std_of_columns(system_covariates_to_plot)
     system_covariates_dim_labels = ["N"]
-elif system_covariate_type == "north_security":
+elif system_covariate_type == "four_security":
     system_covariates_to_plot = snip.system_covariates_zero_to_hundred
     system_covariates_dim_labels = ["N", "E", "S", "W"]
     system_covariates = normalize_matrix_by_mean_and_std_of_columns(system_covariates_to_plot)
@@ -115,6 +123,18 @@ elif system_covariate_type == "running_vulnerability_to_north":
     system_covariates_dim_labels = ["running vulnerability"]
 else:
     raise ValueError("What is the system covariate type?")
+
+###
+# Update recurrence
+###
+
+import copy
+
+
+supra_model = copy.copy(circle_model_JAX)
+supra_model.transform_of_continuous_state_vector_before_premultiplying_by_recurrence_matrix_JAX = (
+    lambda x_vec: jnp.ones_like(x_vec)
+)
 
 
 ####
@@ -162,14 +182,13 @@ print(f"\nELBO after init: {elbo_init:.02f}")
 
 
 ### Are the system-level states related to security scores? : Plots
-s_hat = np.array(results_init.record_of_most_likely_system_states[:, -1], dtype=int)
+s_hat_init = np.array(results_init.record_of_most_likely_system_states[:, -1], dtype=int)
 fig, ax = plot_time_series_with_regime_panels(
     system_covariates_to_plot,
-    s_hat,
+    s_hat_init,
     snip.clock_times,
     dim_labels=system_covariates_dim_labels,
 )
-plt.ylabel("Security scores")
 plt.tight_layout
 fig.savefig(save_dir + "system_segmentations_with_security_scores_after_init.pdf")
 if show_plots_after_init:
@@ -180,7 +199,7 @@ if show_plots_after_init:
 for compass_direction in range(4):
     # Compute the point-biserial correlation coefficient
     corr, pval = stats.pointbiserialr(
-        s_hat, snip.system_covariates_zero_to_hundred[:, compass_direction]
+        s_hat_init, snip.system_covariates_zero_to_hundred[:, compass_direction]
     )
     # Print the correlation coefficient and p-value
     print(
@@ -199,7 +218,7 @@ polar_plot_the_soldier_headings_with_learned_segmentations(
     likely_soldier_regimes_init,
     save_dir,
     basename_prefix="init",
-    show_plot=True,
+    show_plot=show_plots_after_init,
 )
 panel_plot_the_soldier_headings_with_learned_segmentations(
     snip.squad_angles,
@@ -207,7 +226,7 @@ panel_plot_the_soldier_headings_with_learned_segmentations(
     likely_soldier_regimes_init,
     save_dir,
     basename_prefix="init",
-    show_plot=True,
+    show_plot=show_plots_after_init,
 )
 
 report_on_directional_attractors(params_init)
@@ -246,7 +265,6 @@ fig, ax = plot_time_series_with_regime_panels(
     snip.clock_times,
     dim_labels=system_covariates_dim_labels,
 )
-plt.ylabel("Security scores")
 plt.tight_layout
 fig.savefig(save_dir + "system_segmentations_with_security_scores_after_learning.pdf")
 if show_plots_after_init:
@@ -257,10 +275,18 @@ if show_plots_after_init:
 likely_soldier_regimes = compute_likely_soldier_regimes(VEZ_summaries.expected_regimes)
 
 polar_plot_the_soldier_headings_with_learned_segmentations(
-    snip.squad_angles, snip.clock_times, likely_soldier_regimes, save_dir, show_plot=True
+    snip.squad_angles,
+    snip.clock_times,
+    likely_soldier_regimes,
+    save_dir,
+    show_plot=show_plots_after_learning,
 )
 panel_plot_the_soldier_headings_with_learned_segmentations(
-    snip.squad_angles, snip.clock_times, likely_soldier_regimes, save_dir, show_plot=True
+    snip.squad_angles,
+    snip.clock_times,
+    likely_soldier_regimes,
+    save_dir,
+    show_plot=show_plots_after_learning,
 )
 
 ###

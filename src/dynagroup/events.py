7a019632461bdc9b2ed5ef5f1ef365835d825d40
mikewojnowicz <mwojnowi@uci.edu>
2023-05-17 02:39:28 -0600

    (bugfix) utility function in events checking if events have only one member.

--- src/dynagroup/events.py
+++ src/dynagroup/events.py
@@ -70,7 +70,9 @@ def event_end_times_are_proper(event_end_times: NumpyArray1D, T: int) -> Optiona
 
 
 def only_one_event(event_end_times: Optional[NumpyArray1D], T: int) -> Optional[bool]:
-    return (event_end_times is None) or (event_end_times == [-1, T])
+    return (event_end_times is None) or (
+        len(event_end_times) == 2 and (event_end_times == [-1, T]).all()
+    )
 
 
 def get_initialization_times(event_end_times: NumpyArray1D) -> NumpyArray1D:

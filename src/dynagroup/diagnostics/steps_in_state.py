26f50904cd4d6fe84c0f2afc9d3176b91a623caf
mikewojnowicz <mwojnowi@uci.edu>
2023-09-28 12:44:50 -0400

    (maint) some refactoring of steps_in_state so that a core computation (of getting the responses and predictors for entity j while in entity state k) can be reused regardless of whether we want to make plots or compute the mean discrete derivative

--- src/dynagroup/diagnostics/steps_in_state.py
+++ src/dynagroup/diagnostics/steps_in_state.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from typing import Optional
 
 import matplotlib.pyplot as plt
@@ -7,6 +8,30 @@ from dynagroup.sample_weights import (
     make_sample_weights_which_mask_the_initial_timestep_for_each_event,
 )
 from dynagroup.types import NumpyArray1D, NumpyArray2D, NumpyArray3D
+
+
+###
+# Structs
+###
+@dataclass
+class EntityDataInState:
+    """
+    Gives data for entity j=1,...,J when in entity state k=1,...,K
+
+    The responses_jk and predictors_jk both have shape (T_*, D),
+    where T_* depends on how many observations were assigned to state k for entity j.
+
+    Note that the --responses-- are what are assigned to the entity states;
+    the predictors are simply the data from the timestep immediately preceding those responses.
+    """
+
+    responses_jk: NumpyArray2D
+    predictors_jk: NumpyArray2D
+
+
+###
+# Functions
+###
 
 
 def plot_steps_assigned_to_state(
@@ -46,9 +71,64 @@ def plot_steps_assigned_to_state(
     if show_plot:
         plt.show()
     if save_dir is not None:
-        plt.savefig(
-            save_dir + f"{basename_prefix}_steps_assigned_to_state_for_entity_{j}_state_{k}.pdf"
-        )
+        plt.savefig(save_dir + f"{basename_prefix}_steps_assigned_to_state_for_entity_{j}_state_{k}.pdf")
+
+
+def get_entity_data_within_examples_and_assigned_to_entity_state(
+    continuous_states: NumpyArray3D,
+    continuous_state_labels: NumpyArray2D,
+    example_end_times: Optional[NumpyArray1D],
+    use_continuous_states: Optional[NumpyArray2D],
+    j: int,
+    k: int,
+):
+    """
+    Arguments:
+        continuous_states: has shape (T,J,D)
+        continuous_state_labels:  has shape (T,J).
+            Gives a label to the continuous state for each (t,j).
+        example_end_times: optional, has shape (E+1,)
+            Provides `example` boundaries, which allows us to interpret a time series of shape (T,J,:)
+            as (T_grand,J,:), where T_grand is the sum of the number of timesteps across i.i.d "examples".
+            An example boundary might be induced by a large time gap between timesteps, and/or a discontinuity in the continuous states x.
+
+            If there are E examples, then along with the observations, we store
+                end_times=[-1, t_1, …, t_E], where t_e is the timestep at which the e-th example ended.
+            So to get the timesteps for the e-th example, you can index from 1,…,T_grand by doing
+                    [end_times[e-1]+1 : end_times[e]].
+
+        use_continuous_states: If None, we assume all states should be utilized in inference.
+            Otherwise, this is a (T,J) boolean vector such that
+            the (t,j)-th element  is True if continuous_states[t,j] should be utilized
+            and False otherwise.  For any (t,j) that shouldn't be utilized, we don't use
+            that info to do the M-step (on STP, ETP, or CSP), nor the VES step.
+            We leave the VEZ steps as is, though.
+        j: entity index
+        k: entity state index
+    """
+
+    # TODO: It is redundant to compute the sample_weights (for all J,K) each time we call this
+    # (for some j,k). Is there some better way to structure this?  Note however that the runtime
+    # is fast, at least on the Cleveland Starters basketball data, so this consideration might not
+    # be important.
+    sample_weights = make_sample_weights_which_mask_the_initial_timestep_for_each_event(
+        continuous_states,
+        example_end_times,
+        use_continuous_states,
+    )
+
+    timesteps_assigned_to_jk_regardless_of_sample_weight = np.where(continuous_state_labels[:, j] == k)[0]
+    timesteps_with_nonzero_sample_weight = np.where(sample_weights[:, j])[0]
+    # Find timesteps for entity j assigned to label k which also have nonzero sample weight.
+    timesteps_assigned_to_jk = np.sort(
+        list(set(timesteps_assigned_to_jk_regardless_of_sample_weight) & set(timesteps_with_nonzero_sample_weight))
+    )
+    if len(timesteps_assigned_to_jk) > 0:
+        responses_jk = np.array(continuous_states[timesteps_assigned_to_jk, j])
+        predictors_jk = np.array(continuous_states[timesteps_assigned_to_jk - 1, j])
+    else:
+        responses_jk, predictors_jk = [], []
+    return EntityDataInState(responses_jk, predictors_jk)
 
 
 def plot_steps_within_examples_assigned_to_each_entity_state(
@@ -85,31 +165,23 @@ def plot_steps_within_examples_assigned_to_each_entity_state(
         K: number of possible labels
     """
 
-    sample_weights = make_sample_weights_which_mask_the_initial_timestep_for_each_event(
-        continuous_states,
-        example_end_times,
-        use_continuous_states,
-    )
-
     J = np.shape(continuous_states)[1]
     for j in range(J):
         for k in range(K):
-            timesteps_assigned_to_jk_regardless_of_sample_weight = np.where(
-                continuous_state_labels[:, j] == k
-            )[0]
-            timesteps_with_nonzero_sample_weight = np.where(sample_weights[:, j])[0]
-            # Find timesteps for entity j assigned to label k which also have nonzero sample weight.
-            timesteps_assigned_to_jk = np.sort(
-                list(
-                    set(timesteps_assigned_to_jk_regardless_of_sample_weight)
-                    & set(timesteps_with_nonzero_sample_weight)
-                )
+            entity_data_in_state = get_entity_data_within_examples_and_assigned_to_entity_state(
+                continuous_states,
+                continuous_state_labels,
+                example_end_times,
+                use_continuous_states,
+                j,
+                k,
             )
-            if len(timesteps_assigned_to_jk) > 0:
-                responses_jk = np.array(continuous_states[timesteps_assigned_to_jk, j])
-                predictors_jk = np.array(continuous_states[timesteps_assigned_to_jk - 1, j])
-            else:
-                responses_jk, predictors_jk = [], []
             plot_steps_assigned_to_state(
-                responses_jk, predictors_jk, j, k, save_dir, show_plot, basename_prefix
+                entity_data_in_state.responses_jk,
+                entity_data_in_state.predictors_jk,
+                j,
+                k,
+                save_dir,
+                show_plot,
+                basename_prefix,
             )

(bugfix) Change initialization so that we fit closed-form tpm, not gradient descent, when there is only one system state.  Note that if there is only one system state there's no gradient descent to do anyways - there's nothing to learn from system covariates into a categorical GLM.   But until this commit, when trying to run the basketball data with L=1, we were getting an error due to a system state prob being exactly 1.0 (which of course it is in that case, by design).

src/dynagroup/model2a/gaussian/initialize.py
@@ -474,7 +474,8 @@ def fit_ARHMM_to_top_half_of_model(
             system_transition_prior = None
 
             ### M-step (STP)
-            if only_one_example(example_end_times, T):
+            num_system_states = np.shape(STP_JAX.Pi)[0]
+            if only_one_example(example_end_times, T) or num_system_states == 1:
                 # TODO: I had written earlier that the VES step has already taken care of the `use_continuous_states` mask.
                 # But I might want to double check that.
                 STP_JAX = run_M_step_for_STP_in_closed_form(STP_JAX, ES_summary, example_end_times)
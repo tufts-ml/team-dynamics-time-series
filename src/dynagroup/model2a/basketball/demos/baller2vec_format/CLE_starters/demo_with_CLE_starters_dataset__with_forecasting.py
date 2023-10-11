(bugfix) Fixed velocity baseline was off by TWO timesteps! It was taking the discrete derivative from the forecasting window.

src/dynagroup/model2a/basketball/demos/baller2vec_format/CLE_starters/demo_with_CLE_starters_dataset__with_forecasting.py
@@ -65,10 +65,6 @@ model_type = Model_Type.Linear_And_Out_Of_Bounds_Entity_Recurrence__and__All_Pla
 K = 10
 L = 5
 
-# Directories
-datetime_as_string = get_current_datetime_as_string()
-save_dir = f"results/basketball/analyses/CLE_ALL_forecasts_after_CAVI_with_L={L}_K={K}_model_type_{model_type.name}_train_{n_train_games_to_use}_val_{n_val_games}_test_{n_test_games}__{datetime_as_string}/"
-
 # Exploratory Data Analysis
 animate_raw_data = False
 
@@ -93,12 +89,17 @@ num_M_step_iters = 50
 alpha_system_prior, kappa_system_prior = 1.0, 10.0
 
 # Forecast_Collection_For_Example
-random_forecast_starting_points = False
+random_forecast_starting_points = True
 n_cavi_iterations_for_forecasting = 5
 n_forecasts_per_example = 20
 n_forecasting_examples_to_analyze = np.inf
 n_forecasting_examples_to_plot = 0
-T_forecast = 30  # note this is an "off-label" compared to what was generated on disk.
+T_forecast = 20  # note this is an "off-label" compared to what was generated on disk.
+
+
+# Directories
+datetime_as_string = get_current_datetime_as_string()
+save_dir = f"results/basketball/analyses/CLE_with_L={L}_K={K}_model_type_{model_type.name}_train_{n_train_games_to_use}_val_{n_val_games}_test_{n_test_games}_CAVI_{n_cavi_iterations}_its__{datetime_as_string}/"
 
 
 ###

src/dynagroup/model2a/gaussian/forecasts.py
@@ -17,7 +17,7 @@ from dynagroup.vi.E_step import run_VES_step_JAX, run_VEZ_step_JAX
 
 
 def get_forecasts_on_test_set_example(
-    continuous_states: Union[JaxNumpyArray2D, JaxNumpyArray3D],
+    continuous_states_for_one_example: Union[JaxNumpyArray2D, JaxNumpyArray3D],
     params_learned: AllParameters_JAX,
     model: Model,
     T_context: int,
@@ -52,9 +52,8 @@ def get_forecasts_on_test_set_example(
     # Upfront computations
     ###
     example_end_times = None
-    use_continuous_states = None
 
-    T_test, J = np.shape(continuous_states)[:2]
+    T_test, J = np.shape(continuous_states_for_one_example)[:2]
 
     if T_context >= T_test:
         raise ValueError(
@@ -70,7 +69,7 @@ def get_forecasts_on_test_set_example(
 
     print("Now initializing E step for the context portion of the test set...")
     DIMS = dims_from_params(params_learned)
-    continuous_states_during_context_window = continuous_states[:T_context]
+    continuous_states_during_context_window = continuous_states_for_one_example[:T_context]
     results_init = smart_initialize_model_2a(
         DIMS,
         continuous_states_during_context_window,
@@ -91,6 +90,7 @@ def get_forecasts_on_test_set_example(
     ###
     # Run E-steps for the context period
     ###
+    use_continuous_states_during_context_window = None
     VES_summary, VEZ_summaries = results_init.ES_summary, results_init.EZ_summaries
 
     for i in range(n_cavi_iterations):
@@ -104,7 +104,7 @@ def get_forecasts_on_test_set_example(
             model,
             example_end_times,
             system_covariates,
-            use_continuous_states,
+            use_continuous_states_during_context_window,
         )
 
         VEZ_summaries_from_context_window = run_VEZ_step_JAX(
@@ -122,7 +122,7 @@ def get_forecasts_on_test_set_example(
     # Forecasting
     ###
     forecasts = make_forecast_collection_for_one_example(
-        continuous_states,
+        continuous_states_for_one_example,
         params_learned,
         model,
         VEZ_summaries_from_context_window,
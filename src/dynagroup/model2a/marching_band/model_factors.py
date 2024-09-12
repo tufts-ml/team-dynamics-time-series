958eeea51d7abe2f769645f8ae732d333f881f0b
Kaitlin Gili <kaitlinmgili@gmail.com>
2024-09-12 08:26:39 -0400

    Everything except for recurrence is working fine. Good Q(S).

--- src/dynagroup/model2a/marching_band/model_factors.py
+++ src/dynagroup/model2a/marching_band/model_factors.py
@@ -2,6 +2,7 @@ from dynagroup.model import Model
 from dynagroup.model2a.figure8.recurrence import (
     transform_of_continuous_state_vector_before_premultiplying_by_entity_recurrence_matrix_JAX,
 )
+from dynagroup.model2a.marching_band.recurrence import cluster_trigger_system_recurrence_transformation, direction_entity_recurrence_transformation
 from dynagroup.model2a.gaussian.model_factors import (
     compute_log_continuous_state_emissions_after_initial_timestep_JAX,
     compute_log_entity_transition_probability_matrices_JAX,
